651fd01d4c0470dfb0acfcee75af2c424677d0f3
jessermiller <iamjaymiller@yahoo.com>
2022-11-03 18:14:49 -0500

    added javascript

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+let firstName = "Jesse";
+let lastName = "Miller";
+var age = 19;
+
 
 // Exercise 2
+firstName = "Jesse";
+lastName = "Miller";
+age = 19;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was created in the year ${createdYear}`);
+// Exercise 4
+let price = 19.99;
+let isOnSale = fale;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
